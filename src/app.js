9112d15652f2783d026a05c316d4ff133f14e784
JeoftonCosta <jeofton_costa@hotmail.com>
2025-10-10 23:54:17 -0300

    AB#23 Subindo rota post

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>
-  res.json({ status: 'ok', env: process.env.NODE_ENV || 'development' })
-);
+app.use(express.json());
+
+let heroes = []
+let nextId = 1;
+
+app.get('/heroes', (req, res) =>{
+  res.json(heroes);
+});
+
+app.post('/heroes', (req, res) =>{
+    const {name, power} = req.body;
+    const hero = { id: nextId++, name, power};
+    heroes.push(hero);
+  res.status(201).json(hero);
+});
 
 app.get('/health', (req, res) => res.send('OK'));
 
